c2b07cdfde9279f96295bcfcb6d9b0a80921c377
Milan Steenwinkel <milan.steenwinkel@navara.nl>
2017-01-04 18:27:09 +0100

    SQL

--- Scripts/EWA-install-0.0.2.sql
+++ Scripts/EWA-install-0.0.2.sql
@@ -1,2 +1,11 @@
 ALTER TABLE `ewa`.`ewa_result` 
 CHANGE COLUMN `user` `user_id` INT(11) NULL DEFAULT NULL ;
+
+ALTER TABLE `ewa`.`ewa_result` 
+CHANGE COLUMN `user` `user_id` INT(11) NULL DEFAULT NULL ;
+
+ALTER TABLE `ewa`.`ewa_result` 
+CHANGE COLUMN `subject_part` `subject_part_id` INT(11) NULL DEFAULT NULL ;
+
+ALTER TABLE `ewa`.`ewa_subject_part` 
+CHANGE COLUMN `parent_subect` `parent_subect_id` INT(11) NULL DEFAULT NULL ;
