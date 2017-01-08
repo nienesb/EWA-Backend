6da1ed3f6e76f1360cefefb4f81cfec9a61f64b7
Milan Steenwinkel <milan.steenwinkel@navara.nl>
2017-01-08 14:06:25 +0100

    Global CORS

--- Ewa_backend/src/main/java/com/ewabackend/config/appConfig.java
+++ Ewa_backend/src/main/java/com/ewabackend/config/appConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -38,5 +39,10 @@ public class appConfig extends WebMvcConfigurerAdapter {
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+    
+    @Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
 
 }
